db47dbf807755367207ed008e5657dbcef2d9368
hhc <2624429550@qq.com>
2025-05-19 21:25:22 +0800

    分类管理模块代码

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -2,7 +2,9 @@ package com.sky.service;
 
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.result.PageResult;
 
 public interface EmployeeService {
 
@@ -16,4 +18,14 @@ public interface EmployeeService {
     /*
      新增员工方法*/
     void save(EmployeeDTO employeeDTO);
+
+    /*
+    员工分页查询方法*/
+    PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
+
+    void startOrStop(Integer status, Long id);
+
+    Employee getById(Integer id);
+
+    void update(EmployeeDTO employeeDTO);
 }
