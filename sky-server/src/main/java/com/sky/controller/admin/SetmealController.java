4876feca04e66123140c24d3d4f7ea2442045f88
hhc <2624429550@qq.com>
2025-05-28 17:19:43 +0800

    用户端接口实现和缓存代码开发

---
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -0,0 +1,86 @@
+package com.sky.controller.admin;
+
+
+import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.result.PageResult;
+import com.sky.result.Result;
+import com.sky.service.SetmealService;
+import com.sky.vo.SetmealVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * 套餐管理
+ */
+@RestController
+@RequestMapping("/admin/setmeal")
+@Api(tags = "套餐相关接口")
+@Slf4j
+public class SetmealController {
+    @Autowired
+    private SetmealService setmealService;
+
+    /**
+     * 新增套餐
+     * @return
+     */
+    @PostMapping
+    @ApiOperation("新增套餐")
+    @CacheEvict(cacheNames = "setmealCache",key = "#setmealDTO.categoryId")
+    public Result save(@RequestBody SetmealDTO setmealDTO){
+        log.info("新增套餐：{}", setmealDTO);
+        setmealService.saveWithDish(setmealDTO);
+        return Result.success();
+    }
+    @ApiOperation("套餐分页查询")
+    @GetMapping("/page")
+    public Result<PageResult> page(SetmealPageQueryDTO setmealPageQueryDTO){
+        log.info("分页查询套餐：{}",setmealPageQueryDTO );
+        PageResult pageResult = setmealService.pageQuery(setmealPageQueryDTO);
+        return Result.success(pageResult);
+    }
+    @DeleteMapping
+    @ApiOperation("批量删除套餐")
+    @CacheEvict(cacheNames = "setmealCache",allEntries = true)
+    public Result delete(@RequestParam List<Long> ids){
+        log.info("批量删除套餐：{}", ids);
+        setmealService.deleteBatch(ids);
+        return Result.success();
+    }
+
+    @GetMapping("/{id}")
+    @ApiOperation("根据id查询套餐")
+    public Result<SetmealVO> getById(@PathVariable Long id){
+        log.info("根据id查询套餐：{}", id);
+        SetmealVO setmealVO = setmealService.getByIdWithFlavor(id);
+
+        return Result.success(setmealVO);
+    }
+
+    @PutMapping
+    @ApiOperation("修改套餐")
+    @CacheEvict(cacheNames = "setmealCache",allEntries = true)
+    public Result update(@RequestBody SetmealDTO setmealDTO){
+        log.info("修改套餐：{}", setmealDTO);
+        setmealService.updateWithFlavor(setmealDTO);
+        return Result.success();
+    }
+
+    @PostMapping("/status/{status}")
+    @ApiOperation("启用禁用套餐")
+    @CacheEvict(cacheNames = "setmealCache",allEntries = true)
+    public Result startOrStop(@PathVariable Integer status , Long id){
+        log.info("启用禁用套餐：{}", status, id);
+        setmealService.startOrStop(status, id);
+        return Result.success();
+    }
+
+}
--- sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
@@ -1,7 +1,10 @@
 package com.sky.mapper;
 
 
+import com.sky.entity.SetmealDish;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -14,4 +17,26 @@ public interface SetmealDishMapper {
      * @return
      */
     List<Long> getSetmealIdsByDishIds(List<Long> dishIds);
+
+    /**
+     * 批量保存套餐和菜品的关联关系
+     * @param setmealDishes
+     */
+    void insertBatch(List<SetmealDish> setmealDishes);
+
+    /**
+     * 根据套餐id查询套餐和菜品的关联数据
+     * @param
+     * @return
+     */
+    @Delete("delete from setmeal_dish where setmeal_id = #{setmealId}")
+    void deleteBySetmealId(Long setmealId);
+
+    /**
+     * 根据套餐id查询套餐和菜品的关联数据
+     * @param id
+     * @return
+     */
+    @Select("select * from setmeal_dish where setmeal_id = #{id}")
+    List<SetmealDish> getBySetmealId(Long id);
 }
